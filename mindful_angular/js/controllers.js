6baf51525b47f91565eca1f963a99371d97de100
melissawimberly <melissa.f.wimberly@gmail.com>
2016-07-09 17:30:49 -0400

    Add list and detail controllers

--- mindful_angular/js/controllers.js
+++ mindful_angular/js/controllers.js
@@ -1,9 +1,17 @@
-var myApp = angular.module('myApp', []);
+var postControllers = angular.module('postControllers', []);
 
-myApp.controller('MyController', function MyController($scope, $http) {
+postControllers.controller('ListController', ["$scope", "$http", function($scope, $http) {
 	$http.get('js/data.json').success(function(data){
 		$scope.posts = data;
-	})
+		$scope.postOrder = "postName";
+	});
 	 
-}
-)
+}]);
+
+postControllers.controller('DetailsController', ["$scope", "$http", "$routeParams", function($scope, $http, $routeParams) {
+	$http.get('js/data.json').success(function(data){
+		$scope.posts = data;
+		$scope.whichItem = $routeParams.itemId;
+	});
+	 
+}]);
